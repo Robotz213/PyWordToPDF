aeeb1d770fde63f724d870ffc4aa6bcc9ce291cb
Nicholas Silva <nicholasazs213@gmail.com>
2023-05-08 09:34:09 -0400

    Removi a necessidade de planilhas .xlsx

--- main.py
+++ main.py
@@ -25,80 +25,32 @@ class Uploadfiles(QWidget, Ui_Upload):
         self.setupUi(self)
         self.setWindowTitle("Convert To PDF")
         self.BuscarPlanilha.clicked.connect(lambda: self.openfile())
-
-        self.IniciarUpload.clicked.connect(lambda: self.initbot(input_filename=self.lineEndr.text()))
+        self.listfiles = []
+        self.IniciarUpload.clicked.connect(lambda: self.initbot(input_filename=self.listfiles))
     
     def openfile(self):
-        input_filename = QtWidgets.QFileDialog.getOpenFileName()[0]
-        self.lineEndr.setText(input_filename)
-                
+        input_filename = QtWidgets.QFileDialog.getOpenFileNames(
+                filter="Arquivo DOCX (*.docx)")[0]
+        for filename in input_filename:
+            self.listfiles.append(filename)
+        input_filenames_str = "\n".join(input_filename)
+        self.lineEndr.setText(input_filenames_str)           
 
     def initbot(self, input_filename):
-            self.p = input_filename
-
-
-            wrkbk_input = openpyxl.load_workbook(filename=self.p)
-            sheet_input = wrkbk_input.active
-                    
-
-            for i in range(1, sheet_input.max_row+1):
-                    cell_obj = sheet_input.cell(row=i, column=1)
-                    if cell_obj.value is not None and cell_obj.value != '':
-                        docx_name = sheet_input.cell(row=i, column=1).value
-
-
-                        wordir = checkdir().find_all(
-                             exefile='winword.exe', 
-                             pathfile=r'c:/Program Files')
-                        if wordir is True:
-                            try:
-                                self.convertpdf(namefile=docx_name)
-                            except Exception as e:
-                                print(e)
-                        else:
-                             self.pdfinsoffice(namefile=docx_name)
-        
-            if i == sheet_input.max_row:
-                print('fim do processo')
-                showinfo('Sucesso!', 'Todos os documentos foram convertidos com sucesso!')
-
-    def convertpdf(self, namefile):
-
-        wdFormatPDF = 17
-        currentdir = Path(self.p).parent.resolve()
-        inputfile = os.path.join(currentdir, '{infile}'.format(infile=namefile))
-        print(inputfile) 
-        removedocx = inputfile.replace(".docx",".pdf")
-        outpatch = removedocx
-        
-        
-        # Cria instancia de um objeto COM para manipular Documentos Word
-        word = comtypes.client.CreateObject('Word.Application')
-
-        # Carrega Arquivo de entrada (.doc)
-        doc = word.Documents.Open(inputfile)
-        print(colored('Convertendo arquivo "{a}"...'.format(a=namefile), 'yellow'))
-
-        try:
             
-            # Salva arquivo de saida em formato .pdf
-            doc.SaveAs(outpatch, FileFormat=wdFormatPDF)
+            
+            for filename in input_filename:
+                self.p = filename
+                docx_name = filename
 
-            # Fecha arquivo de Entrada
-            doc.Close()
+                self.pdfinsoffice(namefile=docx_name)
 
-            # Finaliza instancia do Objeto COM criado
-            word.Quit()
-            print(colored('Documento Convertido com Sucesso!', 'green'))
-
-        except:
-            print(colored('Não foi possível converter o arquivo "{b}"'.formar(b=namefile), 'red'))  
 
     def pdfinsoffice(self, namefile):
 
         path = 'c:\\Program Files\\'
         name = 'soffice.exe'
-
+        
         print(colored('Convertendo arquivo "{a}"...'.format(a=namefile), 'yellow'))
         if sys.platform == 'linux':
             currentdir = Path(self.p).parent.resolve()
@@ -120,7 +72,7 @@ class Uploadfiles(QWidget, Ui_Upload):
                     LIBRE_OFFICE = os.path.join(root, name)
 
             currentdir = Path(self.p).parent.resolve()
-            arquivo_de_entrada = os.path.join(currentdir, '{infile}'.format(infile=namefile))
+            arquivo_de_entrada = namefile
             outfile = os.path.join(currentdir)
             
             try:
